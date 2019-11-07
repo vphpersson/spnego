b43259711a692d7c81bf1d5b493d4ab2ee21800c
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2019-11-07 01:15:53 +0100

    Reworked SPNEGOToken creation.

--- spnego/token_attributes.py
+++ spnego/token_attributes.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass
-from typing import List, Any, Optional, Dict, Iterable, ClassVar, Type, Set
+from typing import List, Any, ClassVar
 from enum import IntEnum
 from abc import ABC, abstractmethod
 
 from asn1.universal_types import Sequence as ASN1Sequence, ObjectIdentifier, BitString, OctetString, Enumerated
 from asn1.oid import OID
-from asn1.tag_length_value_triplet import Tag, TagLengthValueTriplet
 
 
 class NegTokenInitReqFlag(IntEnum):
@@ -33,15 +32,12 @@ class MechType(ObjectIdentifier):
 @dataclass
 class TokenAttribute(ASN1Sequence, ABC):
     required: ClassVar[bool] = False
+    property_name: ClassVar[str] = NotImplemented
 
     @property
     @abstractmethod
     def parsed_value(self) -> Any:
         raise NotImplementedError
-
-    @property
-    def property_name(self) -> str:
-        return 
 
 
 @dataclass
@@ -99,48 +95,3 @@ class NegState(TokenAttribute, ABC):
     @property
     def parsed_value(self) -> NegTokenRespNegState:
         return NegTokenRespNegState(Enumerated.from_tlv_triplet(tlv_triplet=self.elements[0]).int_value)
-
-
-def parse_attribute_elements(
-    token_inner_elements: Iterable[TagLengthValueTriplet],
-    attribute_classes: Iterable[Type[TokenAttribute]]
-) -> Dict[str, Any]:
-
-    valid_tag_to_attribute_class: Dict[Tag, Type[TokenAttribute]] = {
-        attribute_class.tag: attribute_class
-        for attribute_class in attribute_classes
-    }
-    required_tags: Set[Tag] = {attribute_class.tag for attribute_class in attribute_classes}
-
-    tag_to_parsed_value: Dict[Tag, Any] = {}
-
-    previous_tag: Optional[Tag] = None
-    for element in token_inner_elements:
-        # Check if the tag is not a valid attribute tag.
-        if element.tag not in valid_tag_to_attribute_class:
-            # TODO: Use proper exception.
-            raise ValueError
-        # Check if the tag has been observed previously (i.e. there are multiple with the same tag).
-        if element.tag in tag_to_parsed_value:
-            # TODO: Use proper exception.
-            raise ValueError
-        # Check if the tags are in the right order (i.e. have increasing tag values)
-        if previous_tag is not None and element.tag <= previous_tag:
-            # TODO: Use proper exception.
-            raise ValueError
-
-        tag_to_parsed_value[element.tag] = valid_tag_to_attribute_class[element.tag].from_tlv_triplet(
-            tlv_triplet=element
-        ).parsed_value
-
-        previous_tag = element.tag
-
-    # Check if all required attributes (tags) are present.
-    if any(required_tag not in tag_to_parsed_value for required_tag in required_tags):
-        # TODO: Use proper exception.
-        raise ValueError
-
-    return {
-        valid_tag_to_attribute_class[tag]: parsed_value
-        for tag, parsed_value in tag_to_parsed_value
-    }
